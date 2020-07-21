718884214924fdb12408224d5bf26812f32335aa
Genshi0916 <genshi0916@gmail.com>
2020-07-22 01:14:51 +0900

    [修正]formのデザインを修正

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,4 +1,4 @@
-<template>
+<!--><template>
   <v-row justify="center">
     <v-dialog v-model="dialog" persistent max-width="600px">
       <v-card>
@@ -22,12 +22,43 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="blue darken-1" text to='/'>Close</v-btn>
-          <v-btn color="blue darken-1" text to='/' @click="submit">Send</v-btn>
+          <v-btn to="/" color="blue darken-1" text @click="check">Close</v-btn>
+          <v-btn  v-on:click="submit" color="blue darken-1" tex>Send</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
   </v-row>
+</template>-->
+
+<template>
+<v-card>
+  <v-form
+    ref="form"
+    v-model="valid"
+    lazy-validation
+  >
+    <v-text-field
+      v-model="name"
+      label="お名前"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="email"
+      label="メールアドレス"
+      required
+    ></v-text-field>
+
+    <v-text-field
+      v-model="description"
+      label="問い合わせ内容"
+      required
+    ></v-text-field>
+
+      <v-btn to="/" color="blue darken-1" @click="check">Close</v-btn>
+      <v-btn  v-on:click="submit" color="blue darken-1">Send</v-btn>
+  </v-form>
+  </v-card>
 </template>
 
 <script>
@@ -40,9 +71,9 @@ export default {
   }),
   methods:{
     submit:function(){
-      const name = this.name
-      const email = this.email
-      const description = this.description
+      var name = this.name
+      var email = this.email
+      var description = this.description
 
       const payload = {
         text: '問い合わせがありました\n'+
@@ -50,13 +81,16 @@ export default {
               'email:'+email+'\n'+
               '内容:'+description+'\n'
       }
-      const url = 'https://hooks.slack.com/services/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'
+      const url = 'https://hooks.slack.com/services/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'
 
       fetch(url,{
         method:'POST',
         body:JSON.stringify(payload)
       }).then(()=>{
-        alert('送信完了')
+        alert('送信完了');
+        this.name="";
+        this.email="";
+        this.description="";
       })
     }
   }
