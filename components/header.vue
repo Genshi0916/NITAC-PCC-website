6969fa89ab9503c77aee596a0e0fce6e63065293
Genshi0916 <genshi0916@gmail.com>
2020-07-27 00:30:19 +0900

    [修正]修正

---
+++ components/header.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-layout wrap>
+    <v-spacer></v-spacer>
+    <v-btn to='/' text>Home</v-btn>
+    <v-btn
+    v-for="item in items"
+    :key="item.to"
+    :to="item.to" text>{{item.to}}</v-btn>
+  </v-layout>
+</template>
+
+<script>
+export default
+{
+  name:'header',
+  data:()=>({
+    items:[
+      {to:'activities'},
+      {to:'contact'},
+      {to:'about'},
+    ]
+  })
+}
+</script>
+
+<style>
+.link
+{
+  text-decoration:none;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <div class="text-center">
+        <Logo/>
+      </div>
+      <v-card>
+        <v-card-title class="headline">
+          旭川高専パソコン部へようこそ！！！！
+        </v-card-title>
+        <v-card-text>
+          <p>
+            旭川高専パソコン部ではプログラミング、イラスト制作、動画制作など様々な分野での活動を行っています！
+          </p>
+          <p>秀峰会館2階で平日放課後に気ままに活動！パソコン部で待ってます！</p>
+
+        </v-card-text>
+
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo,
+  }
+}
+</script>
